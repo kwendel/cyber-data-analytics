7b0c8fbcaf05a7063b25d1f6aa8084b6779dc89c
Mathieu <mathieupost@gmail.com>
2019-06-04 17:45:51 +0200

    Only take values from september 2016 into account for training2 set

--- anomaly-detection/src/stats.py
+++ anomaly-detection/src/stats.py
@@ -6,6 +6,14 @@ def confusion(df: pd.DataFrame, flagged_indices: pd.Int64Index):
     flags = pd.Series(False, index=df.index)
     flags[flagged_indices] = True
 
+    # if df is dataset form batadal training 2, we only take into account the
+    # values from the first of september for fair comparison, since the attacks
+    # begin in september and the data before is used for training in the ARMA task
+    if df['datetime'].iloc[0].year == 2016:
+        start = 1416  # start at 00h the first of September
+        df = df[start:]
+        flags = flags[start:]
+
     return confusion_matrix(df['flag'], flags)
 
 
