b224a38c7f0b7b2aa7e0f42ca624f223ee4c857f
Kasper Wendel <kasperwendel@gmail.com>
2019-06-18 18:04:35 +0200

    add test script with timing for the two methods

--- data-streams/src/compare.py
+++ data-streams/src/compare.py
@@ -1,6 +1,9 @@
 # %% Init cm sketch
+import random
+import time
 
 from data import infected_filter, get_most_frequent
+from reservoir import Reservoir
 from sketch import CountMinSketch
 
 if __name__ == '__main__':
@@ -10,10 +13,33 @@ if __name__ == '__main__':
     filter = infected_filter(data_path)
     real_distribution = get_most_frequent(filter)
 
-    # Do a count min sketch, and get the estimated counts for infected connections
-    filter = infected_filter(data_path)
-    cm = CountMinSketch(epsilon=0.01, delta=0.01).analyse_stream(filter)
-    cm_distribution = cm.get_distribution(real_distribution.keys())
+    # CountMinSketch
+    def test_cm(epsilon, delta):
+        start = time.time()
 
-    print(real_distribution)
-    print(cm_distribution)
+        filter = infected_filter(data_path)
+        cm = CountMinSketch(epsilon=epsilon, delta=delta).analyse_stream(filter)
+        cm_distribution = cm.get_distribution(real_distribution.keys())
+
+        elapsed = time.time() - start
+
+        return cm_distribution, elapsed
+
+    # Reservoir sampling
+    def test_reservoir(size):
+        start = time.time()
+
+        random.seed(42)
+        filter = infected_filter(data_path)
+        res = Reservoir(filter, size)
+        res_distribution = res.get_distribution()
+
+        elapsed = time.time() - start
+        return res_distribution, elapsed
+
+    cm_dis, cm_t = test_cm(0.01, 0.01)
+    res_dis, res_t = test_reservoir(1000)
+
+    # print(real_distribution)
+    # print(cm_distribution)
+    # print(res_distribution)
